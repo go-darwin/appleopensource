7ee43fd46767c03bcd6a330c0852e137927f1157
Koichi Shiraishi <zchee.io@gmail.com>
2016-11-29 09:38:14 +0900

    appleopensource: Implements Project, Version and Release
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- appleopensource.go
+++ appleopensource.go
@@ -5,62 +5,286 @@
 package appleopensource
 
 import (
-	"io/ioutil"
+	"bytes"
+	"errors"
+	"fmt"
+	"net/url"
+	"path"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/blang/semver"
 )
 
-// ListMode represets a list mode.
-type ListMode string
+const rootURL = "https://opensource.apple.com"
+
+// ResourceType represents a list mode.
+type ResourceType int
 
 const (
-	// ModeTarballs tarballs mode.
-	ModeTarballs ListMode = "tarballs"
-	// ModeSource source mode.
-	ModeSource = "source"
+	// TypeTarballs tarballs type.
+	TypeTarballs ResourceType = 1 << iota
+
+	// TypeSource source type.
+	TypeSource
 )
 
-// ListTarballs return the lists of opensource.apple.com/tarballs
-func ListTarballs() ([]string, error) {
-	return ListPackage(ModeTarballs)
+// String implements a fmt.Stringer interface.
+func (r ResourceType) String() string {
+	switch r {
+	case TypeTarballs:
+		return "tarballs"
+	case TypeSource:
+		return "source"
+	default:
+		return ""
+	}
 }
 
-// ListSource return the lists of opensource.apple.com/source.
-func ListSource() ([]string, error) {
-	return ListPackage(ModeSource)
+// Project represents a apple open source project.
+type Project struct {
+	Name       string
+	Version    string
+	Updated    bool // for release only
+	ComingSoon bool // for release only
 }
 
-// ListPackage base function that return the opensource.apple.com package list.
-func ListPackage(typ ListMode) ([]string, error) {
-	cacheFile, err := cacheFile(string(typ))
+func (p *Project) Tarball() string {
+	return fmt.Sprintf("%s/%s/%s/%s-%s.tar.gz", rootURL, TypeTarballs, p.Name, p.Name, p.Version)
+}
+
+func (p *Project) Source() string {
+	return fmt.Sprintf("%s/%s/%s/%s-%s/", rootURL, TypeSource, p.Name, p.Name, p.Version)
+}
+
+// IndexProject return the index of opensource.apple.com/<typ> HTML DOM tree.
+func IndexProject(typ string) ([]byte, error) {
+	var rtype string
+
+	switch typ {
+	case TypeTarballs.String():
+		rtype = TypeTarballs.String()
+	case TypeSource.String():
+		rtype = TypeSource.String()
+	default:
+		return nil, errors.New("unknown resource type")
+	}
+
+	baseURL, err := url.Parse(rootURL)
+	if err != nil {
+		return nil, err
+	}
+	baseURL.Path = path.Join(baseURL.Path, fmt.Sprint(rtype))
+
+	dom, err := goquery.NewDocument(baseURL.String())
 	if err != nil {
 		return nil, err
 	}
 
-	f, err := ioutil.ReadFile(cacheFile)
+	table, err := dom.Find("body #content > div.column").Html()
 	if err != nil {
 		return nil, err
 	}
 
-	sr := strings.NewReader(string(f))
-	dom, err := goquery.NewDocumentFromReader(sr)
+	return bytes.TrimSpace([]byte(table)), nil
+}
+
+// ListProject parses the buf HTML DOM tree, and return the project list.
+func ListProject(buf []byte) ([]Project, error) {
+	r := bytes.NewReader(buf)
+	dom, err := goquery.NewDocumentFromReader(r)
 	if err != nil {
 		return nil, err
 	}
 
-	packages := dom.Find("#content > div.column > table > tbody")
+	projects := dom.Find("table > tbody > tr")
 
 	// Subtracts the number of <th>, <hr> and "Parent Directory"
-	list := make([]string, packages.Children().Length()-4)
+	list := make([]Project, projects.Length())
 
-	packages.Children().Each(func(_ int, s *goquery.Selection) {
-		if name := s.Find("td a").Text(); name != "" && name[len(name)-1] == byte('/') {
+	projects.Each(func(i int, s *goquery.Selection) {
+		if name := s.Find("td > a").Text(); name != "" && name[len(name)-1] == byte('/') {
 			// Subtracts the count of header's <th> and <hr>, and slice start count is 0.
 			// Also trims the "/" at the end of the name.
-			list[s.Index()-3] = name[:len(name)-1]
+			list[i].Name = name[:len(name)-1]
 		}
 	})
 
 	return list, nil
+}
+
+// IndexVersion return the index of all versions of the project HTML DOM tree.
+func IndexVersion(project, typ string) ([]byte, error) {
+	var rtype string
+
+	switch typ {
+	case TypeTarballs.String():
+		rtype = TypeTarballs.String()
+	case TypeSource.String():
+		rtype = TypeSource.String()
+	default:
+		return nil, errors.New("unknown resource type")
+	}
+
+	baseURL, err := url.Parse(rootURL)
+	if err != nil {
+		return nil, err
+	}
+	baseURL.Path = path.Join(baseURL.Path, fmt.Sprint(rtype), project)
+
+	dom, err := goquery.NewDocument(baseURL.String())
+	if err != nil {
+		return nil, err
+	}
+
+	table, err := dom.Find("body #content > div.column").Html()
+	if err != nil {
+		return nil, err
+	}
+
+	// 6 is 404 not found
+	if len(table) <= 6 {
+		return nil, fmt.Errorf("Not found %s project", project)
+	}
+
+	return bytes.TrimSpace([]byte(table)), nil
+}
+
+var zero = ".0.0"
+
+// ListVersions parses the buf HTML DOM tree, and return the available versions of the project.
+func ListVersions(buf []byte) ([]string, error) {
+	r := bytes.NewReader(buf)
+	dom, err := goquery.NewDocumentFromReader(r)
+	if err != nil {
+		return nil, err
+	}
+
+	versions := dom.Find("table > tbody > tr")
+
+	// Subtracts the number of <th>, <hr> and "Parent Directory"
+	vlist := make([]semver.Version, versions.Length()-4)
+
+	var start int
+	versions.Each(func(i int, s *goquery.Selection) {
+		if name := s.Find("td > a").Text(); name != "" && name != "Parent Directory" {
+			if start == 0 {
+				start = strings.Index(name, "-") + 1
+			}
+
+			var end int
+			switch {
+			case name[len(name)-1] == '/':
+				// Trims the "/" at the end of the name.
+				end = len(name) - 1
+			case name[len(name)-7] == '.':
+				// Trims the ".tar.gz" at the end of the name.
+				end = len(name) - 7
+			}
+
+			// Subtracts the count of header's <th> and <hr>, and slice start count is 0.
+			// Ignored parse miss
+			vlist[i-3], _ = semver.ParseTolerant(name[start:end])
+		}
+	})
+	semver.Sort(vlist)
+
+	list := make([]string, len(vlist))
+	for i, v := range vlist {
+		// Try trims the ".0.0"
+		list[i] = strings.TrimSuffix(v.String(), zero)
+	}
+	return list, nil
+}
+
+// IndexRelease return the index of all releases of the platform.
+func IndexRelease(platform Platform, version string) ([]byte, error) {
+	var prefix string
+
+	switch platform {
+	case MacOS:
+		v, err := semver.ParseTolerant(version)
+		if err != nil {
+			return nil, err
+		}
+		// wtf why does not use unified url?
+		threshold, err := semver.ParseTolerant("10.9")
+		if err != nil {
+			return nil, err
+		}
+		switch v.Compare(threshold) {
+		case 0, 1: // 0 is equal, 1 is greater than threshold
+			prefix = "os-x"
+		case -1: // -1 is less than threshold
+			prefix = "mac-os-x"
+		}
+	case Xcode:
+		prefix = "developer-tools"
+	case IOS:
+		prefix = "ios"
+	case Server:
+		prefix = "os-x-server"
+	default:
+		return nil, errors.New("unknown platform")
+	}
+
+	baseURL, err := url.Parse(rootURL)
+	if err != nil {
+		return nil, err
+	}
+	baseURL.Path = path.Join(baseURL.Path, "release", fmt.Sprintf("%s-%s.html", prefix, strings.Replace(version, ".", "", -1)))
+
+	dom, err := goquery.NewDocument(baseURL.String())
+	if err != nil {
+		return nil, err
+	}
+
+	table, err := dom.Find("body #content > div.column").Html()
+	if err != nil {
+		return nil, err
+	}
+
+	// 6 is 404 not found
+	if len(table) <= 6 {
+		return nil, fmt.Errorf("Not found %s %s release", platform, version)
+	}
+
+	return bytes.TrimSpace([]byte(table)), nil
+}
+
+const ComingSoon = "(coming soon!)"
+
+// ListRelease parses the release buf HTML DOM, and return the Project slice.
+func ListRelease(buf []byte) ([]Project, error) {
+	r := bytes.NewReader(buf)
+	dom, err := goquery.NewDocumentFromReader(r)
+	if err != nil {
+		return nil, err
+	}
+
+	release := dom.Find("table > tbody > tr")
+
+	projects := make([]Project, release.Length())
+
+	release.Each(func(i int, s *goquery.Selection) {
+		// td.project-name is e.g. "xnu-3789.1.32"
+		if data := strings.TrimSpace(s.Find("td.project-name").Text()); data != "" {
+			p := strings.Split(data, "-")
+
+			projects[i].Name = p[0]
+			version := p[1]
+
+			if strings.Contains(version, ComingSoon) {
+				version = strings.TrimSpace(strings.Replace(version, ComingSoon, "", -1))
+				projects[i].ComingSoon = true
+			}
+
+			projects[i].Version = version
+		}
+		if updated := strings.TrimSpace(s.Find("td.project-updated").Text()); updated != "" {
+			projects[i].Updated = true
+		}
+	})
+
+	return projects, nil
 }

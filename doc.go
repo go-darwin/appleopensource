59174db3258baadd18a6a5993db94e0ec465da74
Koichi Shiraishi <zchee.io@gmail.com>
2017-02-24 20:23:59 +0900

    appleopensource: add package document for godoc
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2017 Koichi Shiraishi. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package appleopensource implements an opensource.apple.com resource management.
+package appleopensource

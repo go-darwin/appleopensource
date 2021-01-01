75798816ecdde4c073a024ca83daf78c8be4a88d
Koichi Shiraishi <zchee.io@gmail.com>
2021-01-01 14:41:23 +0900

    cmd/aos/cmd: rollback list flag switch

--- cmd/aos/cmd/list.go
+++ cmd/aos/cmd/list.go
@@ -50,7 +50,7 @@ func (a *aos) newCmdList(ctx context.Context, ioStreams *IOStreams) *cobra.Comma
 
 	f := cmd.Flags()
 	f.BoolVar(&list.source, "source", false, "List the source resources")
-	f.BoolVar(&list.tarballs, "tarballs", true, "List the tarballs resources")
+	f.BoolVar(&list.tarballs, "tarballs", false, "List the tarballs resources")
 
 	return cmd
 }
@@ -78,12 +78,14 @@ func (l *list) indexList(ctx context.Context, typ appleopensource.ResourceType) 
 func (l *list) run(ctx context.Context) error {
 	var mode appleopensource.ResourceType
 	switch {
+	case l.source && l.tarballs:
+		return errors.New("--source and --tarballs flags are must be one")
 	case l.source:
 		mode = appleopensource.SourceResource
 	case l.tarballs:
 		mode = appleopensource.TarballsResource
-	case l.source && l.tarballs:
-		return errors.New("--source and --tarballs flags are must be one")
+	default:
+		mode = appleopensource.TarballsResource // default is tarballs mode
 	}
 
 	index, err := l.indexList(ctx, mode)

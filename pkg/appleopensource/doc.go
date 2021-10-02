fdc22943b4055bdc062131c77821ad2d411c54a3
Koichi Shiraishi <zchee.io@gmail.com>
2021-10-03 00:16:38 +0900

    all: remove old implementations
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- pkg/appleopensource/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2017 The appleopensource Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package appleopensource implements an opensource.apple.com resource management.
-package appleopensource
